cbbad932ded03f2098bb56272b451fd8b225cfac
Note <note_kap@hotmail.com>
2025-07-16 13:23:13 +0700

    style: standardize quotes in app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { getCurrentDate } from './utils/utils';
-import * as moment from 'moment';
+import { Injectable } from "@nestjs/common";
+import { getCurrentDate } from "./utils/utils";
+import * as moment from "moment";
 
 @Injectable()
 export class AppService {
   getHello(): any {
     return {
+      appName: "Kawasaki Handheld Backend",
       newDate: new Date(),
       getCurrentDate: getCurrentDate(),
       getCurrentDate_toISOString: getCurrentDate().toISOString(),
-      moment: moment().format('YYYY-MM-DD HH:mm:ss'),
+      moment: moment().format("YYYY-MM-DD HH:mm:ss"),
       moment_toISOString: moment().toISOString(),
-      moment_utc: moment().utc().format('YYYY-MM-DD HH:mm:ss'),
+      moment_utc: moment().utc().format("YYYY-MM-DD HH:mm:ss"),
       moment_utc_toISOString: moment().utc().toISOString(),
     };
     // return 'Hello World!';
